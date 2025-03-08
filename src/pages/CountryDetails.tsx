e20060238da5fb41ef7e52d7a53c95e97e7bac6a
Anthony Trépanier <anthony.trepanier@hotmail.com>
2025-03-08 11:10:33 -0500

    fix responsive on country details page

--- src/pages/CountryDetails.tsx
+++ src/pages/CountryDetails.tsx
@@ -32,61 +32,78 @@ const CountryDetails = () => {
   return (
     <div className="py-12">
       {country && (
-        <div className="wrapper flex flex-col space-y-10">
-          <Link
-            to="/"
-            className="rounded-md shadow flex flex-row bg-white dark:bg-dark-blue max-w-30 px-6 py-1 space-x-1 cursor-pointer"
-          >
-            <BiLeftArrowAlt className="w-6 h-6" />
-            <span>Back</span>
-          </Link>
-          {/** Flag */}
-          <div>
-            <img src={country.flag} alt={countryName} />
-          </div>
-          <div className="flex flex-col space-y-6">
-            <h1 className="font-bold text-2xl">{country.name}</h1>
-            <div className="flex flex-col space-y-2">
-              <CountryInfo title={"Native name"} info={country.nativeName} />
-              <CountryInfo title={"Population"} info={country.population} />
-              <CountryInfo title={"Region"} info={country.region} />
-              <CountryInfo title={"Sub region"} info={country.subregion} />
-              <CountryInfo title={"Capital"} info={country.capital} />
+        <div className="wrapper flex flex-col space-y-10 md:flex-row md:items-center md:space-x-12">
+          <div className="space-y-10">
+            <Link
+              to="/"
+              className="rounded-md shadow flex flex-row bg-white dark:bg-dark-blue max-w-30 px-6 py-1 space-x-1 cursor-pointer"
+            >
+              <BiLeftArrowAlt className="w-6 h-6" />
+              <span>Back</span>
+            </Link>
+            {/** Flag */}
+            <div>
+              <img src={country.flag} alt={countryName} />
             </div>
           </div>
-          <div className="flex flex-col space-y-2">
-            <CountryInfo
-              title={"Top level domain"}
-              info={country.topLevelDomain}
-            />
-            <CountryInfo
-              title={"Currencies"}
-              info={getComplexData(country, "currencies", "name")}
-            />
-            <CountryInfo
-              title={"Languages"}
-              info={getComplexData(country, "languages", "name")}
-            />
-          </div>
-          <div>
-            {/** Countries borders */}
-            {country.borders && (
-              <>
-                <h2 className="text-xl capitalize font-bold">
-                  Border countries:
-                </h2>
-                <div className="flex flex-row justify-start flex-wrap  pt-2 space-x-2 space-y-2">
-                  {country.borders.map((border) => (
-                    <div
-                      key={border}
-                      className="shadow bg-white px-6 py-1 rounded-md dark:bg-dark-blue"
-                    >
-                      {border}
-                    </div>
-                  ))}
+
+          <div className="flex flex-col space-y-12">
+            <h1 className="font-bold text-2xl hidden md:flex">
+              {country.name}
+            </h1>
+            <div className="flex flex-col space-y-12 md:flex-row md:space-y-0 md:space-x-12">
+              {/** First info section */}
+              <div className="flex flex-col space-y-6">
+                <h1 className="font-bold text-2xl md:hidden">{country.name}</h1>
+                <div className="flex flex-col space-y-2">
+                  <CountryInfo
+                    title={"Native name"}
+                    info={country.nativeName}
+                  />
+                  <CountryInfo title={"Population"} info={country.population} />
+                  <CountryInfo title={"Region"} info={country.region} />
+                  <CountryInfo title={"Sub region"} info={country.subregion} />
+                  <CountryInfo title={"Capital"} info={country.capital} />
                 </div>
-              </>
-            )}
+              </div>
+
+              {/** Second info section */}
+              <div className="flex flex-col space-y-2">
+                <CountryInfo
+                  title={"Top level domain"}
+                  info={country.topLevelDomain}
+                />
+                <CountryInfo
+                  title={"Currencies"}
+                  info={getComplexData(country, "currencies", "name")}
+                />
+                <CountryInfo
+                  title={"Languages"}
+                  info={getComplexData(country, "languages", "name")}
+                />
+              </div>
+            </div>
+
+            {/** Countries borders */}
+            <div>
+              {country.borders && (
+                <>
+                  <h2 className="text-xl capitalize font-bold">
+                    Border countries:
+                  </h2>
+                  <div className="flex flex-row justify-start flex-wrap  pt-2 space-x-2 space-y-2">
+                    {country.borders.map((border) => (
+                      <div
+                        key={border}
+                        className="shadow bg-white px-6 py-1 rounded-md dark:bg-dark-blue"
+                      >
+                        {border}
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       )}
